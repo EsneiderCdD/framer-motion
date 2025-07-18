8169fa2f7b621adc9e6d8e9410f735649d27a811
EsneiderCdD <ecd.97@hotmail.com>
2025-07-17 23:23:50 -0500

    feat: toggle slide X y Y

--- src/components/SummationProp/ScaleRotate.jsx
+++ src/components/SummationProp/ScaleRotate.jsx
@@ -1,31 +1,29 @@
-import React from 'react';
 import { motion } from 'framer-motion';
-import styles from './Summation.module.css'; // Usa el nuevo module CSS
-import m from '../../assets/images/m.png'; // O una imagen adecuada
+import styles from './Summation.module.css';
+import m from '../../assets/images/m.png';
+import { useState } from 'react';
 
 export default function ScaleRotate() {
+  const [showCode, setShowCode] = useState(false);
+
   const codeString = `<motion.div
   className={styles.circleWrapper}
   initial={{ scale: 0.5, rotate: 0 }}
   animate={{ scale: 1.5, rotate: 360 }}
-  transition={{
-    duration: 4,
-    repeat: Infinity,
-    repeatType: 'reverse',
-    ease: 'easeInOut'
-  }}
+  transition={{ repeat: Infinity, repeatType: 'reverse',
+                duration: 4, ease: 'easeInOut' }}
 >
-  <img src={m} alt="Animación combinada" className={styles.circle} />
+  <img src={m} alt="Animación combinada" 
+  className={styles.circle} />
 </motion.div>`;
 
   return (
     <div className={styles.box}>
       <div className={styles.item}>
-        <pre className={styles.codeBlock}>
-          <code>{codeString}</code>
-        </pre>
+        <button onClick={() => setShowCode(prev => !prev)}>
+          {showCode ? 'i' : '<>'}
+        </button>
       </div>
-
       <div className={styles.item}>
         <motion.div
           className={styles.circleWrapper}
@@ -41,15 +39,20 @@ export default function ScaleRotate() {
           <img src={m} alt="Animación combinada" className={styles.circle} />
         </motion.div>
       </div>
-
       <div className={styles.item}>
-        <p>
-          En este ejemplo combinamos dos propiedades: <strong>scale</strong> y <strong>rotate</strong>. 
-          Mientras el elemento rota completamente sobre su eje, también cambia su tamaño de forma progresiva.
-          <br /><br />
-          Esta combinación crea una sensación dinámica de expansión y giro al mismo tiempo, enriqueciendo 
-          la animación visual sin necesidad de desplazamiento en el eje X o Y.
-        </p>
+        {showCode ? (
+          <pre className={styles.codeBlock}>
+            <code>{codeString}</code>
+          </pre>
+        ) : (
+          <p>
+            En este ejemplo combinamos dos propiedades: <strong>scale</strong> y <strong>rotate</strong>.
+            Mientras el elemento rota completamente sobre su eje, también cambia su tamaño de forma progresiva.
+            <br /><br />
+            Esta combinación crea una sensación dinámica de expansión y giro al mismo tiempo, enriqueciendo
+            la animación visual sin necesidad de desplazamiento en el eje X o Y.
+          </p>
+        )}
       </div>
     </div>
   );
