93923891c349224454c6a6e639188209fe5552e5
EsneiderCdD <ecd.97@hotmail.com>
2025-07-27 11:01:41 -0500

    feat: index.jsx

---
+++ src/components/Variants/index.jsx
@@ -0,0 +1,21 @@
+// src/components/Variants/index.jsx
+
+import VariantStagger from './VariantStagger';
+import VariantControl from './VariantControl';
+import VariantCondition from './VariantCondition';
+import VariantSequence from './VariantSequence';
+
+import styles from './styles.module.css';
+
+export default function VariantsCatalog() {
+  return (
+    <div className={styles.container}>
+      <h4 style={{ fontSize: '2.5rem' }}>Variants</h4>
+
+      <VariantStagger />
+      <VariantControl />
+      <VariantCondition />
+      <VariantSequence />
+    </div>
+  );
+}
