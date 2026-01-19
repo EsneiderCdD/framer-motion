6b83ca295d6d4d0c0126e12274ba1caa29c352a1
EsneiderCdD <ecd.97@hotmail.com>
2026-01-19 06:03:31 -0500

    refactor: move basicos/meta.js to Showcase/Basic/data

---
+++ src/Showcase/Basic/data/meta.js
@@ -0,0 +1,156 @@
+import SlideX from '@/animations/basicos/SlideX';
+import SlideY from '@/animations/basicos/SlideY';
+import SlideXY from '@/animations/basicos/SlideXY';
+import Circle from '../Circular/Circle';
+import CirclePerfect from '../Circular/CirclePerfect';
+import Mov from '../Utility/Duration';
+import RandomMotion from '@/animations/basicos/RandomMotion';
+import LabMotionBuilder from '@/animations/basicos/playground/LabMotionBuilder';
+
+import { generateCircleKeyframes } from '@/utils/generateCircleKeyframes';
+
+const circleMotion = generateCircleKeyframes(40, 100);
+
+export const catalogContent = [
+    { type: 'title', text: 'Movimientos' },
+
+    {
+        type: 'animation',
+        id: 'slide-x',
+        titulo: 'Movimiento eje X ',
+        descripcion: 'Desplazamiento horizontal animado en bucle.',
+        componente: SlideX,
+        animation: {
+            initial: { x: -100 },
+            animate: { x: 100 },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                ease: 'easeInOut'
+            }
+        }
+    },
+    {
+        type: 'animation',
+        id: 'slide-y',
+        titulo: 'Movimiento eje Y',
+        descripcion: 'Desplazamiento vertical animado en bucle.',
+        componente: SlideY,
+        animation: {
+            initial: { y: -100 },
+            animate: { y: 100 },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                ease: 'easeInOut'
+            }
+        }
+    },
+    {
+        type: 'animation',
+        id: 'slide-xy',
+        titulo: 'X y Y',
+        componente: SlideXY,
+        descripcion: 'Desplazamiento vertical y horizontal en diagonal.',
+        animation: {
+            initial: { x: -100, y: -100 },
+            animate: { x: 100, y: 100 },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                ease: 'easeInOut'
+            }
+        }
+    },
+
+
+    {
+        type: 'animation',
+        id: 'circle',
+        titulo: 'Vuelta',
+        componente: Circle,
+        descripcion: 'El logo se mueve en una trayectoria circular infinita.',
+        animation: {
+            initial: { x: 0, y: 0 },
+            animate: {
+                x: [0, 70, 100, 70, 0, -70, -100, -70, 0],
+                y: [-100, -70, 0, 70, 100, 70, 0, -70, -100]
+            },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                ease: 'linear'
+            }
+        }
+    },
+    {
+        type: 'animation',
+        id: 'circle-perfect',
+        titulo: 'Vuelta Perfecta',
+        componente: CirclePerfect,
+        descripcion: 'El logo se mueve en una trayectoria circular infinita con keyframes más suaves.',
+        animation: {
+            initial: { x: circleMotion.x[0], y: circleMotion.y[0] },
+            animate: { x: circleMotion.x, y: circleMotion.y },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                ease: 'linear'
+            }
+        }
+    },
+
+    { type: 'title', text: 'Duration' },
+
+    {
+        type: 'animation',
+        id: 'comparacion-slide-x-y',
+        titulo: 'Duración',
+        descripcion: 'Duracion de la alta y baja en una animación',
+        componente: Mov,
+        animation1: {
+            initial: { y: -100 },
+            animate: { y: 100 },
+            transition: {
+                duration: 2.5,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                ease: 'easeInOut'
+            }
+        },
+        animation2: {
+            initial: { y: -100 },
+            animate: { y: 100 },
+            transition: {
+                duration: 10,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                ease: 'easeInOut'
+            }
+        }
+    },
+    {
+        type: 'animation',
+        id: 'random-motion',
+        titulo: 'Movimiento Errante',
+        descripcion: 'Movimiento aleatorio continuo con velocidad y rango personalizados.',
+        componente: RandomMotion,
+        random: true,
+        speed: 0.5,    // Medio segundo entre cada salto
+        rangeX: 50,   // Movimiento máximo en X de +/- 150px
+        rangeY: 50,   // Movimiento máximo en Y de +/- 150px
+    },
+    {
+        type: 'animation',
+        id: 'Framer-Motion-Lab',
+        titulo: 'Framer Motion Lab',
+        descripcion: 'Intectua con un panel de animaciones',
+        componente: LabMotionBuilder,
+        animation: {
+            animate: { x: 0 },
+        }
+    }
+];
--- src/data/themes.js
+++ src/data/themes.js
@@ -1,4 +1,4 @@
-import { catalogContent as basicos } from '../animations/basicos/meta';
+import { catalogContent as basicos } from '@/Showcase/Basic/data/meta';
 import { catalogContent as gestures } from '../animations/gestures/meta';
 
 export const themes = {
