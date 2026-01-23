b123ac1e6bde76162705fb31400a59f485e57db0
EsneiderCdD <ecd.97@hotmail.com>
2026-01-22 23:57:17 -0500

    feature css in slide x

--- src/Showcase/Basic/Properties/RotateX/index.jsx
+++ src/Showcase/Basic/Properties/RotateX/index.jsx
@@ -3,25 +3,23 @@ import styles from './styles/RotateX.module.css';
 import { rotateXContent } from './data/content';
 
 function RotateX() {
-    const { title, subtitle, code, description } = rotateXContent;
+    const { title, description } = rotateXContent;
 
     return (
         <AnimationBox
             initial={{ rotateX: 0 }}
             animate={{ rotateX: 360 }}
             transition={{
-                duration: 5, repeat: Infinity, repeatType: 'reverse'
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'reverse'
             }}
         >
             <h2>{title}</h2>
-            <h3>{subtitle}</h3>
             <div className={styles.content}>
                 <div className={styles.txt}>
                     {description}
                 </div>
-                <pre className={styles.codeBlock}>
-                    <code>{code}</code>
-                </pre>
             </div>
         </AnimationBox>
     );
--- src/Showcase/Basic/Properties/RotateY/index.jsx
+++ src/Showcase/Basic/Properties/RotateY/index.jsx
@@ -3,25 +3,23 @@ import styles from './styles/RotateY.module.css';
 import { rotateYContent } from './data/content';
 
 function RotateY() {
-    const { title, subtitle, code, description } = rotateYContent;
+    const { title, description } = rotateYContent;
 
     return (
         <AnimationBox
             initial={{ rotateY: 0 }}
             animate={{ rotateY: 360 }}
             transition={{
-                duration: 5, repeat: Infinity, repeatType: 'reverse'
+                duration: 5,
+                repeat: Infinity,
+                repeatType: 'reverse'
             }}
         >
             <h2>{title}</h2>
-            <h3>{subtitle}</h3>
             <div className={styles.content}>
                 <div className={styles.txt}>
                     {description}
                 </div>
-                <pre className={styles.codeBlock}>
-                    <code>{code}</code>
-                </pre>
             </div>
         </AnimationBox>
     );
